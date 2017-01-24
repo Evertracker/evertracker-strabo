44d2428633e8aa5fa143f0e3c376e75602676524
Ioannis Papikas <papikas.ioan@gmail.com>
2017-01-24 16:31:33 +0100

    Change file signature

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,13 +1,12 @@
 <?php
 
 /*
- * This file is part of the Path Segmentation Tracker package.
+ * This file is part of the Path Segmentation (Strabo) package.
  *
- * (c) Ioannis Papikas <papikas.ioan@gmail.com>
+ * (c) Evertracker GmbH <developers@evertracker.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 
 require __DIR__ . '/../vendor/autoload.php';
